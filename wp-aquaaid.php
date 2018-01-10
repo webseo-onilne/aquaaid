0f09e075c52482f48c6d19620d82a2aab06db19a
Web SEO Online <code@webseo.co.za>
2018-01-10 12:00:23 +0200

    Update wp-aquaaid.php

--- wp-aquaaid.php
+++ wp-aquaaid.php
@@ -7,7 +7,7 @@ Author: Web SEO Online (PTY) LTD
 Author URI: https://webseo.co.za
 Version: 0.0.1
 
-	Copyright: © 2016 Web SEO Online (PTY) LTD (email : michael@webseo.co.za)
+	Copyright: © 2016 Web SEO Online (PTY) LTD (email : supprt@webseo.co.za)
 	License: GNU General Public License v3.0
 	License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
